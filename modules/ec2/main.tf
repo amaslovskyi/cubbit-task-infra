6b8d14fee4afc3f008a40e168545aa96aecd368d
Andrii Maslovskyi <amaslovskyi@eatery.club>
2024-08-07 10:33:15 +0300

    try: fix sg

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -7,7 +7,8 @@ resource "aws_instance" "this" {
   ami           = var.ami_id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = concat(var.security_group_ids, [aws_security_group.k3s.id])
+  # vpc_security_group_ids = concat(var.security_group_ids, [aws_security_group.k3s.id])
+  vpc_security_group_ids = var.security_group_ids
   subnet_id              = var.subnet_id
   iam_instance_profile   = var.iam_instance_profile
   key_name               = aws_key_pair.this.key_name
