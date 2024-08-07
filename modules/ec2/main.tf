bb485304fdfc15c055cd53282777254389f4d4d7
Andrii Maslovskyi <amaslovskyi@eatery.club>
2024-08-07 09:44:37 +0300

    add: ssh pk to connect & open port

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,3 +1,8 @@
+resource "aws_key_pair" "this" {
+  key_name   = "${var.common_tags.Project}-${var.common_tags.Environment}-key"
+  public_key = var.public_key
+}
+
 resource "aws_instance" "this" {
   ami           = var.ami_id
   instance_type = var.instance_type
@@ -5,6 +10,7 @@ resource "aws_instance" "this" {
   vpc_security_group_ids = concat(var.security_group_ids, [aws_security_group.k3s.id])
   subnet_id              = var.subnet_id
   iam_instance_profile   = var.iam_instance_profile
+  key_name               = aws_key_pair.this.key_name
 
   user_data = <<-EOF
               #!/bin/bash
