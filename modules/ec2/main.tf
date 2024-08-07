17e2c14580cf0aeb781f5620f82d6356c19d92eb
Andrii Maslovskyi <amaslovskyi@eatery.club>
2024-08-07 09:22:04 +0300

    add: k3s sg to ec2

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "this" {
   ami           = var.ami_id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = var.security_group_ids
+  vpc_security_group_ids = concat(var.security_group_ids, [aws_security_group.k3s.id])
   subnet_id              = var.subnet_id
   iam_instance_profile   = var.iam_instance_profile
 
@@ -15,18 +15,6 @@ resource "aws_instance" "this" {
     var.common_tags,
     {
       Name = "${var.common_tags.Project}-${var.common_tags.Environment}-instance"
-    }
-  )
-}
-
-resource "aws_eip" "this" {
-  # domain   = "vpc"
-  instance = aws_instance.this.id
-
-  tags = merge(
-    var.common_tags,
-    {
-      Name = "${var.common_tags.Project}-${var.common_tags.Environment}-eip"
     }
   )
 }
